a6cf1d7ce3bde9ac48177f7ef61f453f80a41c00
Mateus Oliveira <mateusaugustooliveira@gmail.com>
2019-01-29 22:17:06 -0200

    start implementing fronend rules

--- front_end/js/app.js
+++ front_end/js/app.js
@@ -1,63 +1,94 @@
-function loadCanvasGRid(xd,yd, direction) {
-    var c_canvas = document.getElementById("c");
-    var context = c_canvas.getContext("2d");
+var c_canvas;
+var context;
 
-    var background = new Image();
-    background.src = "images/delete.png";
+function loadCanvasGRid(xd, yd, direction) {
+    c_canvas = document.getElementById("c");
+    context = c_canvas.getContext("2d");
 
-    for (var x = 0; x <= 600; x += 100 ) {
+
+    for (var x = 0; x <= 600; x += 100) {
         context.moveTo(x, 0);
         context.lineTo(x, 600);
-
-        context.lineWidth = 0;
-        context.strokeStyle = "#000000";
-        // Text value at that point
-        context.font = '9px Arial';
-        context.textAlign = 'start';
-        context.fillText(x / 100, 1 * x + 40, 15);
+        buildFillTextInGrid(context, x, 1 * x + 40, 25);
     }
 
-    for (var y = 0; y < 600; y += 100) {
+    for (var y = 0; y <= 600; y += 100) {
         context.moveTo(0, y);
         context.lineTo(600, y);
-
-        context.lineWidth = 0;
-        context.strokeStyle = "#000000";
-        // Text value at that point
-        context.font = '9px Arial';
-        context.textAlign = 'start';
-
-        context.fillText(y /  100, 10, 1 * y + 30);
+        buildFillTextInGrid(context, y, 10, 1 * y + 30);
     }
 
-    var background = new Image();
-    background.src = "https://cdn4.iconfinder.com/data/icons/24x24-free-pixel-icons/24/Delete.png";
-
-    background.onload = function() {
-       // c_canvas.getContext('2d').drawImage(background, 1 * 100 - 60, 1 * 100 - 60);
-
-        c_canvas.getContext('2d').drawImage(background, 440, 540);
-    }
+    renderRobotInGrid(xd, yd, direction);
 
     context.strokeStyle = "#ddd";
     context.stroke();
+
+
 }
 
-loadCanvasGRid();
+function renderImageInGrid(gridPosition) {
+    var plusPixelToRenderInCenter = 15;
 
-function test() {
+    if (gridPosition === 0) {
+        return plusPixelToRenderInCenter;
+    }
 
-    var test = {"commands":["POSITION 0 0 NORTH","FORWARD 1"]};
+    return (gridPosition * 100) + plusPixelToRenderInCenter;
+
+}
+
+function renderRobotInGrid(xd, yd, direction) {
+    var background = new Image();
+    background.src = "images/" + direction + ".png";
+    background.onload = function () {
+        c_canvas.getContext('2d').drawImage(background, renderImageInGrid(xd), renderImageInGrid(yd));
+    }
+}
+
+
+function buildFillTextInGrid(context, grid, marginLeft, topMargin) {
+    if (grid < 600) {
+        context.fillText(grid / 100, marginLeft, topMargin);
+    }
+}
+
+function resetCanvas() {
+    context.clearRect(0, 0, 600, 600);
+}
+
+
+function buildResultInView(result) {
+    var parsedData = JSON.parse(result.responseText);
+    document.getElementById("grid").innerHTML = '(' + parsedData.grid.x + ',' + parsedData.grid.y + ')';
+    document.getElementById("direction").innerHTML = parsedData.direction;
+    var link = document.getElementById('result');
+    link.style.display = 'block'; //or
+}
+
+
+function IsJsonString(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        alert("json not valid")
+        return false;
+    }
+    return true;
+}
+
+function sendScript(script) {
+    if (!IsJsonString(script)) return false;
+
+    resetCanvas();
 
     $.ajax({
             type: 'POST',
             url: 'http://localhost:8080/robot',
             contentType: 'application/json',
-            data: JSON.stringify(test),
+            data: script,
             dataType: 'html',
             complete: function (result) {
-                //$cardBody.removeClass('isLoading');
-
+                buildResultInView(result);
             }
         }
     ).done(function (result) {
@@ -72,7 +103,16 @@ function test() {
     return false;
 }
 
-//test();
+function init() {
+    var script = {"commands": ["POSITION 1 3 EAST", "FORWARD 3", "RIGHT", "RIGHT", "FORWARD 1", "RIGHT", "FORWARD 2"]};
+    document.getElementById("script").innerHTML = JSON.stringify(script, undefined, 2);
+    //Default robot position
+    loadCanvasGRid(0, 0, 'SOUTH');
+}
+
+
+init();
+
 
 
 
